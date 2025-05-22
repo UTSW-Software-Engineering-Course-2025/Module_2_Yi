0ce31dd893a8d4119cef357279863e90dd1496ed
Austin Marckx <austin.marckx@utsouthwestern.edu>
2025-05-22 15:16:01 -0500

    push bugfix

--- 03_CondaEnvironment/activate_montillo_conda_env.sh
+++ 03_CondaEnvironment/activate_montillo_conda_env.sh
@@ -4,7 +4,7 @@ export TF_USE_LEGACY_KERAS=1
 ### Use this with training accounts
 # Setup GPU based onining account (even: 0; odd: 1)
 account=$(whoami | grep -Eo '[1-9][0-9]*')
-if [ ! -z $account]
+if [ ! -z $account ];
 then 
 	export CUDA_VISIBLE_DEVICES=$($account%2)
     echo "Assigned GPU: $CUDA_VISIBLE_DEVICES"
