4306737e2e203c440061b6599b57abe5d07b0275
Austin Marckx <austin.marckx@utsouthwestern.edu>
2025-05-22 14:28:32 -0500

    push training account auto GPU assign

--- 03_CondaEnvironment/activate_montillo_conda_env.sh
+++ 03_CondaEnvironment/activate_montillo_conda_env.sh
@@ -1,4 +1,12 @@
-# install Day 1 environment into jupyter python
+# Setup GPU based on training account (even: 0; odd: 1)
+# Ex: train00
+export TF_CPP_MIN_LOG_LEVEL=2
+export TF_USE_LEGACY_KERAS=1
+export CUDA_VISIBLE_DEVICES=$(($(whoami | grep -Eo '[1-9][0-9]*')%2))
+echo "My assigned GPU is card: $CUDA_VISIBLE_DEVICES"
+
+# Conda Env
 module load python
 conda activate /project/nanocourse/SWE_OOP/shared/CondaEnvs/
 module unload python
+
