46b6ad5d2bd756d29002b4cce841d0f23e8b4a81
Austin Marckx <austin.marckx@utsouthwestern.edu>
2025-05-22 15:00:53 -0500

    push updated environ exports

--- 03_CondaEnvironment/activate_montillo_conda_env.sh
+++ 03_CondaEnvironment/activate_montillo_conda_env.sh
@@ -1,12 +1,19 @@
-# Setup GPU based on training account (even: 0; odd: 1)
-# Ex: train00
 export TF_CPP_MIN_LOG_LEVEL=2
 export TF_USE_LEGACY_KERAS=1
-export CUDA_VISIBLE_DEVICES=$(($(whoami | grep -Eo '[1-9][0-9]*')%2))
-echo "My assigned GPU is card: $CUDA_VISIBLE_DEVICES"
 
-# Conda Env
+### Use this with training accounts
+# Setup GPU based onining account (even: 0; odd: 1)
+account=$(whoami | grep -Eo '[1-9][0-9]*')
+if [ ! -z $account]
+then 
+	export CUDA_VISIBLE_DEVICES=$($account%2)
+    echo "Assigned GPU: $CUDA_VISIBLE_DEVICES"
+else
+	echo "Unable to set CUDA_VISIBLE_DEVICES from training account. Setting to 0"
+	export CUDA_VISIBLE_DEVICES=0	
+fi
+
+# install Day 1 environment into jupyter python
 module load python
 conda activate /project/nanocourse/SWE_OOP/shared/CondaEnvs/
 module unload python
-
